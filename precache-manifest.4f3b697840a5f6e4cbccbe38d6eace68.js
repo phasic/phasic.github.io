9134961fe8bdfe5a1a29cf195274bd77bf274ef2
yoran <yoran@cobben.com>
2019-10-01 15:39:16 +0200

    new

--- precache-manifest.4f3b697840a5f6e4cbccbe38d6eace68.js
+++ precache-manifest.4f3b697840a5f6e4cbccbe38d6eace68.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "9850f50792002ea4c835",
+    "revision": "7d2e3bd3f068bc885840",
     "url": "main.css"
   },
   {
-    "revision": "9850f50792002ea4c835",
-    "url": "main.9850f507.js"
+    "revision": "7d2e3bd3f068bc885840",
+    "url": "main.7d2e3bd3.js"
   },
   {
-    "revision": "d9aa71fee0d530fa8cc7c26be7657d32",
+    "revision": "bf042d9e68a609920a647ad526fc7ef9",
     "url": "index.html"
   },
   {
     "revision": "8391957aaff84ed2243736878b5b1108",
     "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
+    "url": "vendor/webcomponents-loader.js"
   },
   {
     "revision": "893102e1869adc5bce0a94feb6f74249",
@@ -22,10 +26,6 @@ self.__precacheManifest = [
   {
     "revision": "0f9a5cf89f1d4757a6a4945a329e1fc0",
     "url": "vendor/bundles/webcomponents-ce.js"
-  },
-  {
-    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
-    "url": "vendor/webcomponents-loader.js"
   },
   {
     "revision": "449e154f5a93678178caf6d1d5857d65",
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.614e84d75475ebf796467042381066ce.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.4f3b697840a5f6e4cbccbe38d6eace68.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
