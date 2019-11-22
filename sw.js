a952050d2233d129cd6556b2312edb6483c1672e
yoran <yoran@cobben.com>
2019-11-22 16:12:50 +0100

    bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.877a25653eaf7829c4ab04cc29f5a3ee.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.9369aaabffdbdc20b480449585cd456b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
 
